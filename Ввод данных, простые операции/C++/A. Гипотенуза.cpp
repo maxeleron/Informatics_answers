2a898c881437ff8a5c1c88616c8fabd09249b8f9
maxeleron <maxeleron@gmail.com>
2016-07-30 22:55:44 +0300

    2 задачи в Ввод-вывод С++

---
+++ Ввод данных, простые операции/C++/A. Гипотенуза.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+	int a,b;
+	cin>>a>>b;
+	cout<<pow(a*a + b*b,1.0/2.0);
+	return 0;
+}
---
+++ Ввод данных, простые операции/C++/B. Число -1 и +1.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	int a;
+	cin>>a;
+	cout<<"The next number for the number "<<a<<" is "<<a+1<<"."<< endl;
+	cout<<"The previous number for the number "<<a<<" is "<<a-1<<".";
+	system("pause");
+	return 0;
+}
+
